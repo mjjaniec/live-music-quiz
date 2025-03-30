cd53ea3319edfe25e8af8c8efc5fafe88353be46
Michał Janiec <mjjaniec@gmail.com>
2025-03-30 16:08:57 +0200

    Url updated + deployment script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+host="ec2-user@ec2-52-59-243-95.eu-central-1.compute.amazonaws.com"
+
+#./mvnw clean package -Pproduction
+ssh -i "lmq.pem" $host "sudo systemctl stop lmq.service"
+scp -i "lmq.pem" target/live-music-quiz-1.0-SNAPSHOT.jar $host:/opt/lmq-service/app.jar
+ssh -i "lmq.pem" $host "sudo systemctl start lmq.service"
