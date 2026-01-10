3748090cc65d1e4cb8fd8fd4e7626346bc227d7f
Michał Janiec <mjjaniec@gmail.com>
2026-01-10 12:14:31 +0100

    headless

--- src/test/java/com/github/mjjaniec/lmq/WrapUpGuiVerificationIT.java
+++ src/test/java/com/github/mjjaniec/lmq/WrapUpGuiVerificationIT.java
@@ -82,7 +82,7 @@ public class WrapUpGuiVerificationIT {
     @BeforeAll
     static void launchBrowser() {
         playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @AfterAll
